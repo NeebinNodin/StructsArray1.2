0d7ccf9f0bd47fc019b5ddce5b10d138382d2352
NeebinNodin <164468785+NeebinNodin@users.noreply.github.com>
2024-09-11 23:17:24 -0500

    Stuff

--- NB.StructsArray1.2/Main.cpp
+++ NB.StructsArray1.2/Main.cpp
@@ -10,6 +10,7 @@ struct Student
 {
 	string Name;
 	float GPA = 0;
+	int Courses[3];
 };
 
 int main()
@@ -17,18 +18,20 @@ int main()
 	const int NUM_STUDENTS = 2;
 	Student students[NUM_STUDENTS] = 
 	{
-		{ "Jim", 3.2 },
-		{ "Sue", 3.8 }
+		{ "Jim", 3.2, {1, 2, 5} },
+		{ "Sue", 3.8, {3, 4, 5} }
 	};
 
 	float sum = 0;
 	for (int i = 0; i < NUM_STUDENTS; i++)
 	{
-		cout << students[i].Name << " has a GPA of " << students[i].GPA << "\n";
+		cout << students[i].Name << " has a GPA of " << students[i].GPA << " - ";
+		for (int j = 0; j < 3; j++) cout << students[i].Courses[j] << ", ";
+		cout << "\n";
 		sum += students[i].GPA;
 	}
 
-	cout << "The average GPA is: " << (sum / NUM_STUDENTS) << "\n";
+	cout << "The average GPA is: " << (sum / NUM_STUDENTS);
 	
 	(void)_getch();
 	return 0;
