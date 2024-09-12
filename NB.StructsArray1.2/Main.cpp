a3daed8dde3bb37d2febbefa1f71a3cec855823b
NeebinNodin <164468785+NeebinNodin@users.noreply.github.com>
2024-09-11 23:44:02 -0500

    Almost done.

--- NB.StructsArray1.2/Main.cpp
+++ NB.StructsArray1.2/Main.cpp
@@ -6,34 +6,60 @@
 
 using namespace std;
 
-struct Student
+struct Course
 {
 	string Name;
-	float GPA = 0;
-	int Courses[3];
+	int Credits;
 };
 
 int main()
 {
-	const int NUM_STUDENTS = 2;
-	Student students[NUM_STUDENTS] = 
+	const int NUM_COURSES = 4;
+	Course courses[NUM_COURSES] =
 	{
-		{ "Jim", 3.2, {1, 2, 5} },
-		{ "Sue", 3.8, {3, 4, 5} }
+		{"Intro to Programming", 3},
+		{"Data Structures", 4},
+		{"Algorithms", 4},
+		{"Intermediate Programming", 3}
 	};
 
-	float sum = 0;
-	for (int i = 0; i < NUM_STUDENTS; i++)
+	for (int i = 0; i < NUM_COURSES; i++)
 	{
-		cout << students[i].Name << " has a GPA of " << students[i].GPA << " - ";
-		for (int j = 0; j < 3; j++) cout << students[i].Courses[j] << ", ";
-		cout << "\n";
-		sum += students[i].GPA;
+		cout << "Courses: " << courses[i].Name << "\n";
+		cout << "Credits: " << courses[i].Credits << "\n\n";
 	}
 
-	cout << "The average GPA is: " << (sum / NUM_STUDENTS);
-	
 	(void)_getch();
 	return 0;
-}
+};
+		//struct Student
+		//{
+		//	string Name;
+		//	float GPA = 0;
+		//	int Courses[3];
+		//};
+
+		//int main()
+		//{
+		//	const int NUM_STUDENTS = 2;
+		//	Student students[NUM_STUDENTS] = 
+		//	{
+		//		{ "Jim", 3.2, {1, 2, 5} },
+		//		{ "Sue", 3.8, {3, 4, 5} }
+		//	};
+		//
+		//	float sum = 0;
+		//	for (int i = 0; i < NUM_STUDENTS; i++)
+		//	{
+		//		cout << students[i].Name << " has a GPA of " << students[i].GPA << " - ";
+		//		for (int j = 0; j < 3; j++) cout << students[i].Courses[j] << ", ";
+		//		cout << "\n";
+		//		sum += students[i].GPA;
+		//	}
+		//
+		//	cout << "The average GPA is: " << (sum / NUM_STUDENTS);
+		//	
+		//	(void)_getch();
+		//	return 0;
+		//}
 
